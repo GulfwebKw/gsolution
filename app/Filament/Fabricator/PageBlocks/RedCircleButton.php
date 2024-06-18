59667402a7712576f10b569e8ea335c70710ef1d
Erfan Ebrahimi <me@erfanebrahimi.ir>
2024-06-18 16:35:12 +0330

    add red circle button

---
+++ app/Filament/Fabricator/PageBlocks/RedCircleButton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Fabricator\PageBlocks;
+
+use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\TextInput;
+use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
+
+class RedCircleButton extends PageBlock
+{
+    public static function getBlockSchema(): Block
+    {
+        return Block::make('red-circle-button')
+            ->schema([
+                TextInput::make('title'),
+                TextInput::make('redTitle'),
+                TextInput::make('buttonTitle')->required(),
+                TextInput::make('buttonLink')->url()->required(),
+                TextInput::make('backgroundText'),
+            ]);
+    }
+
+    public static function mutateData(array $data): array
+    {
+        return $data;
+    }
+}
