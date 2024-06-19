28aa3912e7297b8d26ef03c847d87b2ed32da2d8
Erfan Ebrahimi <me@erfanebrahimi.ir>
2024-06-19 08:36:10 +0330

    fix database and data

---
+++ resources/views/layouts/breadcrumb.blade.php
@@ -0,0 +1,15 @@
+<!-- Page Banner Start -->
+<section class="page-banner-area overlay pt-220 rpt-150 pb-170 rpb-100 rel z-1 bgs-cover text-center" style="background-image: url({{ asset('assets/images/banner/banner-bg.jpg') }});">
+    <div class="container">
+        <div class="banner-inner rpt-10">
+            <h2 class="page-title wow fadeInUp delay-0-2s">Web Design</h2>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-center wow fadeInUp delay-0-4s">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Web Design</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</section>
+<!-- Page Banner End -->
---
+++ resources/views/layouts/headTag.blade.php
@@ -0,0 +1,29 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<!-- Title -->
+<title>G Solutions -Proffesinal Web Design and Development Services Kuwait.</title>
+<!-- Favicon Icon -->
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/x-icon">
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+
+<!-- Flaticon -->
+<link rel="stylesheet" href="{{ asset('assets/css/flaticon.min.css') }}">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('assets/css/fontawesome-5.14.0.min.css') }}">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.min.css') }}">
+<!-- Nice Select -->
+<link rel="stylesheet" href="{{ asset('assets/css/nice-select.min.css') }}">
+<!-- Animate -->
+<link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+<!-- Slick -->
+<link rel="stylesheet" href="{{ asset('assets/css/slick.min.css') }}">
+<!-- Main Style -->
+<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
